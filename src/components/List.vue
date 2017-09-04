156f4141ae48127c831bdb47b7a295201fd1cce1
Adithya Reddy <adithyakreddy6@gmail.com>
2017-09-04 23:08:40 +0530

    spaces formatting

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,14 @@
 <template>
-    <ul>
-        <ListItem v-for="tab of tabs" :tab="tab" :key="tab.id"></ListItem>
-    </ul>
+  <ul>
+    <ListItem v-for="tab of tabs" :tab="tab" :key="tab.id"></ListItem>
+  </ul>
 </template>
 
 <script>
 import ListItem from './ListItem.vue';
 
 export default {
-    props: ['tabs'],
-    components: { ListItem }
+  props: ['tabs'],
+  components: { ListItem }
 };
 </script>
--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,17 +1,17 @@
 <template>
-    <li>
-        {{ tab.id }} - {{ tab.title }} -
-        <button @click="focus(tab)">Focus</button>
-    </li>
+  <li>
+    {{ tab.id }} - {{ tab.title }} -
+    <button @click="focus(tab)">Focus</button>
+  </li>
 </template>
 
 <script>
 export default {
-    props: ['tab'],
-    methods: {
-        focus(tab) {
-            chrome.tabs.update(tab.id, { active: true });
-        }
+  props: ['tab'],
+  methods: {
+    focus(tab) {
+      chrome.tabs.update(tab.id, { active: true });
     }
+  }
 };
 </script>
