49773ee2e0565e5272b7bc167ec37111fb129de5
Adithya Reddy <adithyakreddy6@gmail.com>
2017-09-04 22:42:00 +0530

    list and focus tabs

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <List :tabs="tabs">
+    </List>
+</template>
+
+<script>
+import List from './components/List.vue';
+
+export default {
+    data() {
+        return {
+            tabs: []
+        };
+    },
+    mounted() {
+        chrome.tabs.query({}, tabs => {
+            this.tabs = tabs;
+        });
+    },
+    components: { List }
+}
+</script>
