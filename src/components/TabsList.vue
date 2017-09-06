ab2c05fdb7672b2f98fb3ac0e272bd5e27c4b4f3
Adithya Reddy <adithyakreddy6@gmail.com>
2017-09-06 09:07:59 +0530

    refactor tabsList and TabsItem into TabsList

--- src/components/TabsList.vue
+++ src/components/TabsList.vue
@@ -1,19 +1,34 @@
 <template>
   <v-list two-line subheader>
-    <ListItem v-for="tab of tabs" :tab="tab" :key="tab.id"></ListItem>
+    <v-card v-for="tab of tabs" :key="tab.id" class="list-item" hover @click="focus(tab)">
+      <v-list-tile>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ tab.title }}</v-list-tile-title>
+          <v-list-tile-sub-title>
+            <a :href="tab.url">{{ tab.url }}</a>
+          </v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-card>
   </v-list>
 </template>
 
 <script>
-import ListItem from './ListItem.vue';
-
 export default {
   props: ['tabs'],
-  components: { ListItem }
+
+  methods: {
+    focus(tab) {
+      chrome.tabs.update(tab.id, { active: true });
+    }
+  }
 }
 </script>
 
 <style lang="stylus">
 .list-item
   margin-top 15px
+  background-color #e3ffff
+  &:hover
+    background-color #B3E5FC
 </style>
