67b3f8e19ea8098d9714f82e365d8b86be2b3148
Adithya Reddy <adithyakreddy6@gmail.com>
2017-09-06 21:11:33 +0530

    all new styling for historylist

--- src/components/HistoryList.vue
+++ src/components/HistoryList.vue
@@ -1,12 +1,14 @@
 <template>
   <v-list subheader>
+    <v-subheader>
+      <v-icon>history</v-icon> &nbsp; &nbsp; History
+    </v-subheader>
     <v-list-tile avatar v-for="item of history" :key="item.id" @click="open(item)">
       <v-list-tile-avatar>
-        <v-icon>history</v-icon>
+        <img :src="`chrome://favicon/${item.url}`">
       </v-list-tile-avatar>
       <v-list-tile-content>
-        <v-list-tile-title v-if="item.title">
-          {{ item.title }} -
+        <v-list-tile-title v-if="item.title">{{ item.title }} -
           <span class="url">{{ item.url }}</span>
         </v-list-tile-title>
         <v-list-tile-title v-else>
@@ -14,7 +16,6 @@
         </v-list-tile-title>
       </v-list-tile-content>
     </v-list-tile>
-  </v-list>
 </template>
 
 <script>
