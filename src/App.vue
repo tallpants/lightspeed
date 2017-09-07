0b727deb44b619f7d7bb160eca6720489af764fb
Adithya Reddy <adithyakreddy6@gmail.com>
2017-09-07 15:29:07 +0530

    200ms debounce

--- src/App.vue
+++ src/App.vue
@@ -29,44 +29,48 @@ export default {
       tabs: [],
       bookmarks: [],
       rawHistory: [],
-      filteredHistory: []
+      filteredHistory: [],
+      debounceTimer: null
     };
   },
 
   watch: {
     searchString(newSearchString) {
 
-      // Filter the tabs according to the search string a user types.
-      chrome.tabs.query({}, tabs => {
-        this.tabs = tabs.filter(tab => {
-          return (
-            tab.title.toLowerCase().includes(newSearchString.toLowerCase()) ||
-            tab.url.toLowerCase().includes(newSearchString.toLowerCase())
-          );
-        });
-      });
-
-      if (newSearchString === '') {
-        this.bookmarks = [];
-        this.filteredHistory = [];
-      } else {
-        // Load all the bookmarks that match the search string when the user types
-        chrome.bookmarks.search(newSearchString, bookmarks => {
-          this.bookmarks = bookmarks;
-        });
-
-        // Filter history according to the search string the user types
-        this.filteredHistory = this.rawHistory.filter(historyItem => {
-          if (historyItem.title) {
+      clearTimeout(this.debounceTimer);
+      this.debounceTimer = setTimeout(() => {
+        // Filter the tabs according to the search string a user types.
+        chrome.tabs.query({}, tabs => {
+          this.tabs = tabs.filter(tab => {
             return (
-              historyItem.title.toLowerCase().includes(newSearchString.toLowerCase()) ||
-              historyItem.url.toLowerCase().includes(newSearchString.toLowerCase())
+              tab.title.toLowerCase().includes(newSearchString.toLowerCase()) ||
+              tab.url.toLowerCase().includes(newSearchString.toLowerCase())
             );
-          } else {
-            return historyItem.url.toLowerCase().includes(newSearchString.toLowerCase());
-          }
-        }).slice(0, 20);
-      }
+          });
+        });
+
+        if (newSearchString === '') {
+          this.bookmarks = [];
+          this.filteredHistory = [];
+        } else {
+          // Load all the bookmarks that match the search string when the user types
+          chrome.bookmarks.search(newSearchString, bookmarks => {
+            this.bookmarks = bookmarks;
+          });
+
+          // Filter history according to the search string the user types
+          this.filteredHistory = this.rawHistory.filter(historyItem => {
+            if (historyItem.title) {
+              return (
+                historyItem.title.toLowerCase().includes(newSearchString.toLowerCase()) ||
+                historyItem.url.toLowerCase().includes(newSearchString.toLowerCase())
+              );
+            } else {
+              return historyItem.url.toLowerCase().includes(newSearchString.toLowerCase());
+            }
+          }).slice(0, 20);
+        }
+      }, 200);
     },
   },
 
