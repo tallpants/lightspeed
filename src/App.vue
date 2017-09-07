82f16a7570e59269d886081737a37954faecdcc3
Adithya Reddy <adithyakreddy6@gmail.com>
2017-09-07 14:41:11 +0530

    solved the weird history problem

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
       <v-flex xs8 offset-xs2>
         <TabsList :tabs="tabs"></TabsList>
         <BookmarksList :bookmarks="bookmarks"></BookmarksList>
-        <HistoryList :history="history"></HistoryList>
+        <HistoryList :history="filteredHistory"></HistoryList>
       </v-flex>
     </v-layout>
   </v-container>
@@ -28,43 +28,56 @@ export default {
       searchString: '',
       tabs: [],
       bookmarks: [],
-      history: []
+      rawHistory: [],
+      filteredHistory: []
     };
   },
 
   watch: {
     searchString(newSearchString) {
-      // Load all the bookmarks and history items that match the
-      // search string into state when the user types.
       if (newSearchString === '') {
         this.bookmarks = [];
-        this.history = [];
+        this.filteredHistory = [];
       } else {
+        // Load all the bookmarks that match the search string when the user types
         chrome.bookmarks.search(newSearchString, bookmarks => {
           this.bookmarks = bookmarks;
         });
-        chrome.history.search({ text: newSearchString }, history => {
-          this.history = history;
-        });
-      }
 
-      // Filter the tabs according to the search string a user types.
-      chrome.tabs.query({}, tabs => {
-        this.tabs = tabs.filter(tab => {
-          return (
-            tab.title.toLowerCase().includes(newSearchString.toLowerCase()) ||
-            tab.url.toLowerCase().includes(newSearchString.toLowerCase())
-          );
+        // Filter the tabs according to the search string a user types.
+        chrome.tabs.query({}, tabs => {
+          this.tabs = tabs.filter(tab => {
+            return (
+              tab.title.toLowerCase().includes(newSearchString.toLowerCase()) ||
+              tab.url.toLowerCase().includes(newSearchString.toLowerCase())
+            );
+          });
         });
-      });
-    }
+
+        // Filter history according to the search string the user types
+        this.filteredHistory = this.rawHistory.filter(historyItem => {
+          if (historyItem.title) {
+            return (
+              historyItem.title.toLowerCase().includes(newSearchString.toLowerCase()) ||
+              historyItem.url.toLowerCase().includes(newSearchString.toLowerCase())
+            );
+          } else {
+            return historyItem.url.toLowerCase().includes(newSearchString.toLowerCase());
+          }
+        }).slice(0, 20);
+      }
+    },
   },
 
-  // Load all open tabs into state initially
+  // Load all open tabs and history into state initially
   mounted() {
     chrome.tabs.query({}, tabs => {
       this.tabs = tabs;
     });
+
+    chrome.history.search({ text: '', maxResults: 0, startTime: 0 }, history => {
+      this.rawHistory = history;
+    })
   },
 
   components: { TabsList, BookmarksList, HistoryList }
