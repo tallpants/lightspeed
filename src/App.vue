tallpants/lightspeed

commit 58d97cc74cc720670fe5750a6dc8e0036a54cb6d

Converted search items into hyperlinks. This fixes #4 and enables #5

src/App.vue
@@ -94,6 +94,10 @@ export default {
   mounted() {
     chrome.tabs.query({}, tabs => this.tabs = tabs);
     chrome.history.search({ text: '', maxResults: 0, startTime: 0 }, history => this.fullHistory = history);
+    document.getElementsByClassName('nolink')[0].addEventListener('click', e => {
+      e.preventDefault();
+      e.stopPropagation();
+    });
   },
 
   watch: {

src/components/List.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    <v-list subheader>
+    <v-list class="nolink" subheader>
       <v-subheader>
         <v-icon>tab</v-icon> &nbsp; &nbsp; Tabs
       </v-subheader>
       <v-list-tile class="list-item" avatar v-for="tab of tabs" v-if="tab.url !== 'chrome://newtab/'" :class="{ 'selected': selected === tab.id }" :key="tab.id" @click="focusTab(tab)">
-        <v-list-tile-avatar>
-          <img :src="`chrome://favicon/${tab.url}`">
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          <v-list-tile-title>{{ tab.title}} -
-            <span class="url">{{ tab.url }}</span>
-          </v-list-tile-title>
-        </v-list-tile-content>
+        <a :href="`${tab.url}`">
+          <v-list-tile-avatar>
+            <img :src="`chrome://favicon/${tab.url}`">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ tab.title}} -
+              <span class="url">{{ tab.url }}</span>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </a>
       </v-list-tile>
     </v-list>
 
@@ -20,15 +22,17 @@
       <v-subheader>
         <v-icon>bookmark</v-icon> &nbsp; &nbsp; Bookmarks
       </v-subheader>
-      <v-list-tile class="list-item" avatar v-for="bookmark of bookmarks" :class="{ 'selected': selected === bookmark.id}" :key="bookmark.id" @click="open(bookmark)">
-        <v-list-tile-avatar>
-          <img :src="`chrome://favicon/${bookmark.url}`">
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          <v-list-tile-title>{{ bookmark.title}} -
-            <span class="url">{{ bookmark.url }}</span>
-          </v-list-tile-title>
-        </v-list-tile-content>
+      <v-list-tile class="list-item" avatar v-for="bookmark of bookmarks" :class="{ 'selected': selected === bookmark.id}" :key="bookmark.id">
+        <a :href="`${bookmark.url}`">
+          <v-list-tile-avatar>
+            <img :src="`chrome://favicon/${bookmark.url}`">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ bookmark.title}} -
+              <span class="url">{{ bookmark.url }}</span>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </a>
       </v-list-tile>
     </v-list>
 
@@ -36,18 +40,20 @@
       <v-subheader>
         <v-icon>history</v-icon> &nbsp; &nbsp; History
       </v-subheader>
-      <v-list-tile class="list-item" avatar v-for="historyItem of history" :class="{ 'selected': selected === historyItem.id }" :key="historyItem.id" @click="open(historyItem)">
-        <v-list-tile-avatar>
-          <img :src="`chrome://favicon/${historyItem.url}`">
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          <v-list-tile-title v-if="historyItem.title">{{ historyItem.title }} -
-            <span class="url">{{ historyItem.url }}</span>
-          </v-list-tile-title>
-          <v-list-tile-title v-else>
-            <span class="url">{{ historyItem.url }}</span>
-          </v-list-tile-title>
-        </v-list-tile-content>
+      <v-list-tile class="list-item" avatar v-for="historyItem of history" :class="{ 'selected': selected === historyItem.id }" :key="historyItem.id">
+        <a :href="`${historyItem.url}`">
+          <v-list-tile-avatar>
+            <img :src="`chrome://favicon/${historyItem.url}`">
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title v-if="historyItem.title">{{ historyItem.title }} -
+              <span class="url">{{ historyItem.url }}</span>
+            </v-list-tile-title>
+            <v-list-tile-title v-else>
+              <span class="url">{{ historyItem.url }}</span>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </a>
       </v-list-tile>
     </v-list>
   </div>
@@ -80,6 +86,15 @@ export default {
 .list__tile
   height 48px
   font-size 14px
+
+  &:hover
+    background rgba(0,0,0,.12)
+
+  a
+    display flex
+    align-items center
+    color inherit
+    text-decoration none
 
   .avatar
     min-width 0px

commit c6d1a8261708eb166b9b33a365a8585464297db0

fixed regressions from 58d97cc74cc720670fe5750a6dc8e0036a54cb6d

src/App.vue
@@ -94,10 +94,6 @@ export default {
   mounted() {
     chrome.tabs.query({}, tabs => this.tabs = tabs);
     chrome.history.search({ text: '', maxResults: 0, startTime: 0 }, history => this.fullHistory = history);
-    document.getElementsByClassName('nolink')[0].addEventListener('click', e => {
-      e.preventDefault();
-      e.stopPropagation();
-    });
   },
 
   watch: {

src/components/List.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <v-list class="nolink" subheader>
+    <v-list subheader>
       <v-subheader>
         <v-icon>tab</v-icon> &nbsp; &nbsp; Tabs
       </v-subheader>
-      <v-list-tile class="list-item" avatar v-for="tab of tabs" v-if="tab.url !== 'chrome://newtab/'" :class="{ 'selected': selected === tab.id }" :key="tab.id" @click="focusTab(tab)">
+      <v-list-tile class="list-item" avatar v-for="tab of tabs" v-if="tab.url !== 'chrome://newtab/'" :class="{ 'selected': selected === tab.id }" :key="tab.id" @click.prevent="focusTab(tab)">
         <a :href="`${tab.url}`">
           <v-list-tile-avatar>
             <img :src="`chrome://favicon/${tab.url}`">
@@ -75,6 +75,7 @@ export default {
       window.location = item.url;
     }
   },
+
   props: ['tabs', 'bookmarks', 'history', 'selected']
 }
 </script>
@@ -95,6 +96,8 @@ export default {
     align-items center
     color inherit
     text-decoration none
+    width 100%
+    text-overflow ellipsis
 
   .avatar
     min-width 0px